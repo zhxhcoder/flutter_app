0a6d59d9a76e3a23bd5d98741380a238e9088aa6
zhxh <zhxhcoder@gmail.com>
2018-12-14 17:20:10 +0800

    TestActivity

---
+++ android/app/src/main/kotlin/com/zhxh/flutterapp/TestActivity.kt
@@ -0,0 +1,18 @@
+package com.zhxh.flutterapp
+
+import android.os.Bundle
+import android.util.Log
+import io.flutter.app.FlutterActivity
+
+/**
+ * Created by zhxh on 2018/12/14
+ */
+
+class TestActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //TODO
+        Log.d("zhxhdebug", "onCreate")
+
+    }
+}
